a128a0d6f5a7f6eb0987750d1823fd3e834d8011
Julius <Julius@Gedmis-MacBook-Air.local>
2025-05-29 14:36:00 +0300

    Removed Allowed Hosts setting.

--- commerce/settings.py
+++ commerce/settings.py
@@ -25,9 +25,6 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.243', '127.0.0.1']
-
-
 # Application definition
 
 INSTALLED_APPS = [
